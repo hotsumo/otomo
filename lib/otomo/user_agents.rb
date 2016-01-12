d699d8de3ac938535fbacf8410b6c9205c9947f6
Yacine Petitprez <yacine@kosmogo.fr>
2016-01-12 17:26:58 +0800

    Fix user agents list

--- lib/otomo/user_agents.rb
+++ lib/otomo/user_agents.rb
@@ -1,5 +1,11 @@
 module Otomo
+  # A list of real user agent, just to spoof in case you need it.
   module UserAgents
     CHROME = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+    FIREFOX = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+    FIREFOX_MAC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0"
+    OPERA = "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16"
+    EDGE = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    MSIE = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
   end
 end
