25043b58418eadc6a5c1c2c0c7e7b2c885637933
Yacine Petitprez <yacine.petitprez@ingedata.net>
2016-07-23 12:56:53 +0800

    Fix issue with query path

--- lib/otomo/robot.rb
+++ lib/otomo/robot.rb
@@ -132,9 +132,9 @@ private
 
       if idx = path.index("?")
         [ path[0..idx-1],  path[idx+1..-1] ]
+      else
+        [path]
       end
-
-      [path]
     end
 
     def query_encoded_data data, initial=nil
